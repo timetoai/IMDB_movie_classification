e21f7a0afa89af3b2bbc7871e28aabe335208f67
Vladislav <hellovlad1999@mail.ru>
2022-11-19 15:12:05 +0800

    m

--- text_features.py
+++ text_features.py
@@ -5,6 +5,7 @@ import pandas as pd
 import spacy
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from gensim.models import Word2Vec
 
 
 def bag_of_words(texts, cv=None, n_words=1):
@@ -37,3 +38,33 @@ def spacy_approach(texts, nlp=None):
     base_shape = nlp("nlp").vector.shape
     text2vector = lambda x: nlp(x).vector if x else np.zeros(base_shape)
     return np.row_stack([text2vector(text) for text in texts]), nlp
+
+def word2vec_approach(texts):
+    texts = [ x.split(' ') for x in texts ]
+
+    model = Word2Vec(texts, 
+                        min_count=1,   # Ignore words that appear less than this
+                        size=50,      # Dimensionality of word embeddings
+                        workers=2,     # Number of processors (parallelisation)
+                        window=5,      # Context window for words during training
+                        iter=30)       # Number of epochs training over corpus
+
+    max_len = 0
+    for sentence in texts:
+        if len(sentence) > max_len:
+            max_len = len(sentence)
+
+    text_embeds = []
+    for sentence in texts:
+        sentence_emb = []
+        sent_len = len(sentence)
+        for word in sentence:
+            sentence_emb.append(model.wv[word])
+        
+        base = (max_len-sent_len) * 50 * [0.0]
+        if sent_len < max_len:
+            text_embeds.append(base + list(np.concatenate(sentence_emb)))
+        else:
+            text_embeds.append(np.concatenate(sentence_emb))
+
+    return np.row_stack(text_embeds), model
